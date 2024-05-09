1df711b0d4afc215a38944e0971d0c89ba7661be
Samir Akhmed <warrior.owl@yandex.ru>
2024-05-10 00:34:47 +0300

    Some logging messages

--- notification_service/src/main_grpc.py
+++ notification_service/src/main_grpc.py
@@ -2,16 +2,17 @@ import asyncio
 import logging
 
 import grpc
+from src.brokers.rabbit_message_broker import RabbitMessageBroker
 from src.core.config import settings
 from src.core.grpc import auth_pb2_grpc, file_pb2_grpc, managers_pb2_grpc, ucg_pb2_grpc
 from src.services.grpc.v1.auth import AuthGrpcNotificationService
 from src.services.grpc.v1.films import FilmGrpcNotificationService
 from src.services.grpc.v1.managers import ManagerGrpcNotificationService
 from src.services.grpc.v1.ucgs import UcgGrpcNotificationService
-from src.brokers.rabbit_message_broker import RabbitMessageBroker
 
 
 async def serve() -> None:
+    logging.info("Server starting....")
     server = grpc.aio.server()
     broker = RabbitMessageBroker(
         host=settings.rabbit_host,
@@ -20,7 +21,9 @@ async def serve() -> None:
         password=settings.rabbit_password,
     )
     try:
+        logging.info("Broker dlx infrastructure starting...")
         await broker.idempotency_startup()
+        logging.info("Broker dlx infrastructure setup done")
         auth_pb2_grpc.add_UserNotificationServicer_to_server(
             AuthGrpcNotificationService(rabbit_connection=broker), server
         )
@@ -33,8 +36,10 @@ async def serve() -> None:
         ucg_pb2_grpc.add_UcgNotificationServicer_to_server(
             UcgGrpcNotificationService(message_broker=broker), server
         )
+        logging.info("Added active routes")
         server.add_insecure_port(f"[::]:{settings.grpc_port}")
         await server.start()
+        logging.info("Server ready to receive requests...")
         await server.wait_for_termination()
     finally:
         await broker.idempotency_shutdown()
